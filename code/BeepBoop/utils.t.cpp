3c249e99fd68346420e570525d2ecafed6eee9f2
Max Ismagilov <mismagilov25@students.polytechnic.org>
2023-08-20 17:00:50 -0700

    Add all unit tests

---
+++ code/BeepBoop/utils.t.cpp
@@ -0,0 +1,15 @@
+#include "utils.hpp"
+
+#include <gtest/gtest.h>
+
+struct kv
+{
+  BeepBoop::u64 num;
+  int count;
+} tests[6] = {{0x1ULL, 1}, {0xFULL, 4}, {0x0ULL, 0}, {0xF0000ULL, 4}, {0x3ULL, 2}, {0x5000ULL, 2}};
+
+TEST(Utils, Count) {
+  for (int i = 0; i < 6; ++i) {
+    EXPECT_EQ(BeepBoop::utils::count_bits(tests[i].num), tests[i].count);
+  }
+}
